95a1dd57467c24f0c8f57112ea564e9c4e831955
tinaafang <94018300+tinaafang@users.noreply.github.com>
2022-09-06 00:25:10 -0400

    update

--- appForAdminDetail.js
+++ appForAdminDetail.js
@@ -19,71 +19,71 @@ function idk(dishItem){
 
     $('<div/>',{
         class:'adminDetailItem',
-        id:'adminDetailItem-'+orderID
+        id:'adminDetailItem-'+ID
     }).appendTo('#adminDetail');
 
     $('<div/>',{
         class:'adminDetailItemLeft',
-        id:'adminDetailItemLeft-'+orderID
-    }).appendTo('#adminDetailItem-'+orderID);
+        id:'adminDetailItemLeft-'+ID
+    }).appendTo('#adminDetailItem-'+ID);
 
     $('<p/>',{
         text: dishItem.dishID+" "+dishItem.dishName,
         class: 'adminDetailDishName',
-    }).appendTo('#adminDetailItemLeft-'+orderID);
+    }).appendTo('#adminDetailItemLeft-'+ID);
 
     $('<p/>',{
         text: (dishItem.notes === '') ? '备注：无' :'备注：加'+dishItem.notes,
         class: 'adminDetailDishNote',
-    }).appendTo('#adminDetailItemLeft-'+orderID);
+    }).appendTo('#adminDetailItemLeft-'+ID);
 
     $('<div/>',{
         class:'adminDetailItemRight',
-        id:'adminDetailItemRight-'+orderID
-    }).appendTo('#adminDetailItem-'+orderID);
+        id:'adminDetailItemRight-'+ID
+    }).appendTo('#adminDetailItem-'+ID);
 
     $('<p/>',{
         text: "数量： "+dishItem.dishQuant,
         class: 'adminDetailDishQuant',
-    }).appendTo('#adminDetailItemRight-'+orderID);
+    }).appendTo('#adminDetailItemRight-'+ID);
 
     $('<p/>',{
         text: "$"+dishItem.dishTotalPrice,
         class: 'adminDetailDishNote',
-    }).appendTo('#adminDetailItemRight-'+orderID);
+    }).appendTo('#adminDetailItemRight-'+ID);
 }
 
 $('<div/>',{
     class:"adminOrderBtn",
-    id:"adminOrderBtn-"+data[0].orderID
+    id:"adminOrderBtn-"+data[0].ID
 }).appendTo('#adminDetail');
 
 $("<p/>",{
     text:"$"+data[0].orderPrice,
     class:'adminOrderBtnPrice',
-    id:"adminOrderBtnPrice-"+orderID
-}).appendTo('#adminOrderBtn-'+orderID);
+    id:"adminOrderBtnPrice-"+ID
+}).appendTo('#adminOrderBtn-'+ID);
 
 $("<p/>",{
     text:(Number(data[0].orderStatus) == 0)? '未下单':'已下单',
     class:'adminOrderBtnSubmitOrder',
-    id:"adminOrderBtnSubmitOrder-"+orderID,
+    id:"adminOrderBtnSubmitOrder-"+ID,
     style:(Number(data[0].orderStatus) == 0)? "background-color:rgb(255, 254, 196);":"background-color:#daeb9e;"
-}).appendTo('#adminOrderBtn-'+orderID);
+}).appendTo('#adminOrderBtn-'+ID);
 
 
 
-$("#adminOrderBtn-"+orderID).on('click',function(){
-    if($("#adminOrderBtnSubmitOrder-"+orderID).html()=='未下单'){
-        $("#adminOrderBtnSubmitOrder-"+orderID).html('已下单');
-        $("#adminOrderBtnSubmitOrder-"+orderID).css('background-color','#daeb9e');
+$("#adminOrderBtn-"+ID).on('click',function(){
+    if($("#adminOrderBtnSubmitOrder-"+ID).html()=='未下单'){
+        $("#adminOrderBtnSubmitOrder-"+ID).html('已下单');
+        $("#adminOrderBtnSubmitOrder-"+ID).css('background-color','#daeb9e');
         data.forEach(dishItem => dishItem.orderStatus=1);
     } else{
-        $("#adminOrderBtnSubmitOrder-"+orderID).html('未下单');
-        $("#adminOrderBtnSubmitOrder-"+orderID).css('background-color','rgb(255, 254, 196)');
+        $("#adminOrderBtnSubmitOrder-"+ID).html('未下单');
+        $("#adminOrderBtnSubmitOrder-"+ID).css('background-color','rgb(255, 254, 196)');
         data.forEach(dishItem => dishItem.orderStatus=0);
     }
-    localStorage.setItem('orderData'+orderID,JSON.stringify(data));
+    localStorage.setItem('orderData'+ID,JSON.stringify(data));
     $.redirect('adminPage.php', {status: data[0].orderStatus,orderID:orderID});
     
 })
